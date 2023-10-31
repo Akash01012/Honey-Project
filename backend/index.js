c898749d54acd206b3fe79881f69ee5b5e0af701
Akash Kumar <101648265+Akash01012@users.noreply.github.com>
2023-11-01 01:14:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -93,6 +93,18 @@ app.get("/", (req, res) => {
 });
 
 // Allow cors
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://aimhrs.netlify.app', 'https://aimhrs.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 // const allowedOrigins = ["https://allindiamarketinghrsolution.com"];
 
 // const corsOptions = {
